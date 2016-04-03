a315933e7260eb51ebb52105e9180640e40e4fc8
Joshua Ching <jchingdev@gmail.com>
2016-04-03 18:12:43 -0400

    toolbar button to launch imagepicker

--- Meme Maker/ViewController.swift
+++ Meme Maker/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imagePickerButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openImagePicker(){
+        let imagePicker = UIImagePickerController()
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
 
 }
 
