a1be7e7c23bb700ed900b0d24872939fa24c0e2e
Joshua Ching <jchingdev@gmail.com>
2016-04-03 17:57:41 -0400

    init project

---
+++ Meme Maker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme Maker
+//
+//  Created by Joshua Ching on 2016-04-03.
+//  Copyright © 2016 Joshua Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
