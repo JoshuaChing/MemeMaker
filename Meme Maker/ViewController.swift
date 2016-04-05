8d283fda18f6543fc178be20e0962f6f65cee887
Joshua Ching <jchingdev@gmail.com>
2016-04-04 21:31:35 -0400

    shift frame with keyboard

--- Meme Maker/ViewController.swift
+++ Meme Maker/ViewController.swift
@@ -37,6 +37,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool){
+        super.viewWillAppear(animated)
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotifications()
+    }
 
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
         if textField.text == topTextFieldDefaultText || textField.text == bottomTextFieldDefaultText{
@@ -62,6 +72,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
         imagePicker.sourceType = sourceType
         self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func subscribeToKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "shiftViewForKeyboard:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "unshiftViewForKeyboard:", name: UIKeyboardWillHideNotification, object:nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func shiftViewForKeyboard(notification: NSNotification){
+        self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func unshiftViewForKeyboard(notification: NSNotification){
+        self.view.frame.origin.y += getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
     }
     
     @IBAction func openImagePicker(){
